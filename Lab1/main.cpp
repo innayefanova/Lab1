a6f89c3da618593bcead62ffe4b0bffd9637d1d5
Inna Yefanova <innayefanova@icloud.com>
2023-02-23 23:29:12 -0800

    Initial Commit

---
+++ Lab1/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Lab1
+//
+//  Created by Inna Yefanova on 2/23/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+   // insert code here...
+   std::cout << "Hello, World!\n";
+   return 0;
+}
